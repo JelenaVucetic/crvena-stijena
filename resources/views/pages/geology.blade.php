dd58fa8fb690bddc007304ebcaa1ce5a5dda9852
jVucetic <jelena.vucetic@vegaitglobal.com>
2026-08-13 22:55:36 +0200

    caption translation in progress

--- resources/views/pages/geology.blade.php
+++ resources/views/pages/geology.blade.php
@@ -19,17 +19,23 @@
                                                 <a href="{{asset("images/resource/Geology 2 20190614_100333-min.jpg")}}"
                                                    class="lightbox-image" data-fancybox="Gallery"><img
                                                         src="{{asset("images/resource/Geology 2 20190614_100333-min.jpg")}}" alt=" "></a>
+                                                <p>{{ __('geology-caption-1') }}</p>
                                             </div>
-                                            <div class="image col-md-6 col-sm-6 col-xs-12">
-                                                <a href="{{asset("images/resource/geology.jpg")}}"
-                                                   class="lightbox-image" data-fancybox="Gallery"><img
-                                                        src="{{asset("images/resource/geology.jpg")}}" alt=" "></a>
-                                            </div>
+
                                             <div class="image col-md-6 col-sm-6 col-xs-12">
                                                 <a href="{{asset("images/resource/Geology 1 DJI_0912-min.JPG")}}"
                                                    class="lightbox-image" data-fancybox="Gallery"><img
                                                         src="{{asset("images/resource/Geology 1 DJI_0912-min.JPG")}}" alt=" "></a>
+                                                <p>{{ __('geology-caption-2') }}</p>
                                             </div>
+
+
+                                         {{--   <div class="image col-md-6 col-sm-6 col-xs-12">
+                                                <a href="{{asset("images/resource/geology.jpg")}}"
+                                                   class="lightbox-image" data-fancybox="Gallery"><img
+                                                        src="{{asset("images/resource/geology.jpg")}}" alt=" "></a>
+                                            </div>--}}
+
                                         </div>
                                     </div>
                                 </div>
